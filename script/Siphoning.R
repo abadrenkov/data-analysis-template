69e308d23f94327a0d11476a7065cbd9a4002ffb
Yvonne Ng <ngyvonne@berkeley.edu>
2014-05-03 16:32:34 -0700

    updated siphon

--- script/Siphoning.R
+++ script/Siphoning.R
@@ -1,6 +1,6 @@
 source("Data_Cleaning.R")
-subset <- DFG
-a <- rep(1:151, each=10)
+subset <- DFG[6:755,]
+a <- rep(1:75, each=10)
 subset$Factor <- a
 rownames(subset) <- 1:dim(subset)[1]
 subset$Factor <- factor(subset$Factor)
