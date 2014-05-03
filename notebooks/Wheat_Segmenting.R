abb70d8e2e978a5a4dcaf3931dc455001f139e86
Ivan Kho <ivankho@berkeley.edu>
2014-05-02 17:27:50 -0700

    Segmenting code

---
+++ notebooks/Wheat_Segmenting.R
@@ -0,0 +1,11 @@
+file <- read.xlsx("Wheat_Prices_1790_1850.xlsx", 1)
+segment <- file[-(1:5),c("NA.","NA..23")]
+names(segment) <- c("Year", "Price")
+rownames(segment) <- 1:dim(segment)[1]
+write.xlsx(segment, "Wheat_Prices_1790_1850_2.xlsx", sheetName="Wheat")
+
+file2 <- read.xlsx("Wheat_Prices_1850_1950.xlsx", 1)
+segment <- file2[-(1:7),1:2]
+names(segment) <- c("Year", "Price")
+rownames(segment) <- 1:dim(segment)[1]
+write.xlsx(segment, "Wheat_Prices_1850_1950_2.xlsx", sheetName="Wheat")
